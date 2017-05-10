b75be0bff89105c587a94645019c8818b9055078
skywalker <1158620887@qq.com>
2017-05-10 09:07:25 +0800

    TestPython201705100907

---
+++ src/IO/TestIO.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#coding=utf-8
+'''
+Created on 2017年5月9日
+
+@author: Administrator
+'''
+import os
+with open("../Test/TestTime.py","r") as f:
+    print f.read(100)
+    position=f.tell()
+    print position
+#     position=f.seek(0,0)
+    print f.read(50)
+
+# os.rename("D:/testProject/TestFile/TestFile002.pdf","D:/testProject/TestFile/TestName.pdf")
+# os.remove("D:/testProject/TestFile/TestFile003.ppt")
+# os.mkdir("D:/testProject/TestFile/testDir")
+print os.getcwd()
+os.rmdir("D:/testProject/TestFile/testDir")
+    
--- src/Test/test.py
+++ src/Test/test.py
@@ -6,3 +6,14 @@ from config.config import config
 # cfg=config()
 print config.db_host
 print config.db_passwd
+
+def testFun():
+    str1="hello"
+    print locals()
+    print globals()
+    
+testFun()
+print locals()
+print globals()
+    
+    
